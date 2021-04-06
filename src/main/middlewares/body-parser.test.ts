7d904842d4732fcab49aaeb8966db08e6a964550
Bruno Sousa <brse01@gmail.com>
2021-04-06 09:37:48 -0300

    feat: ensure cors is enabled

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -1,8 +1,10 @@
 import request from 'supertest'
 import app from '../config/app'
-
+interface BodyTypes {
+  name: string
+}
 describe('Body Parser Middleware', () => {
-  const makeBody = (): any => ({ name: 'Bruno' })
+  const makeBody = (): BodyTypes => ({ name: 'Bruno' })
 
   test('Should parse body as json', async () => {
     app.post('/test-body-parser', (req, res) => {
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,2 +1,2 @@
 import app from './config/app'
-app.listen(5050, () => { console.log('Server running at http://localhist:5050') })
+app.listen(5050, () => { console.log('Server running at http://localhost:5050') })
