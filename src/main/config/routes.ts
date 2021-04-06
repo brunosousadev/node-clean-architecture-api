e2c5ccb65981edebf6d1de32f7a733a8a4906a7c
Bruno Sousa <brse01@gmail.com>
2021-04-06 13:14:00 -0300

    refactor: connect to mongodb before integration testes

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -7,7 +7,6 @@ export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
   readdirSync(`${__dirname}/../routes`).map(async file => {
-    console.log(file)
     if (!file.includes('.test.')) {
       (await import(`../routes/${file}`)).default(router)
     }
