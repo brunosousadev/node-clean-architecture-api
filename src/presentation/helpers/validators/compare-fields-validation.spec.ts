7352b952778f6427e961a9cd5fe3d222686da6d9
Bruno Sousa <brse01@gmail.com>
2021-04-08 16:12:20 -0300

    refactor: rename file test

--- src/presentation/helpers/validators/compare-fields-validation.spec.ts
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -2,12 +2,23 @@ import { InvalidParamError } from '../../errors'
 import { CompareFieldsValidation } from './compare-fields-validation'
 
 const makeSut = (): CompareFieldsValidation => {
-  return new CompareFieldsValidation('field', 'fieldToCompare')
+  return new CompareFieldsValidation('field', 'fieldToCompareName')
 }
-describe('RequiredFiedl Validation', () => {
-  test('Should return a InvalidParamError if validation fails', () => {
+describe('CompareFields Validation', () => {
+  test('Should return InvalidParamError if validation fails', () => {
     const sut = makeSut()
-    const error = sut.validate({ field: 'any_value', fieldToCompare: 'wrong_value' })
-    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+    const error = sut.validate({
+      field: 'any_value',
+      fieldToCompareName: 'wrong_value'
+    })
+    expect(error).toEqual(new InvalidParamError('fieldToCompareName'))
+  })
+  test('Should not return  if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({
+      field: 'any_value',
+      fieldToCompareName: 'any_value'
+    })
+    expect(error).toBeFalsy()
   })
 })
